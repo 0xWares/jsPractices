4c004c7b674d16b7be4c02e00ffa019356c65375
0xWares <md.abdulwares.outlook.com>
2026-03-18 07:05:33 +0600

    Capitalize the first word of a string...

--- problem3.js
+++ problem3.js
@@ -4,3 +4,13 @@ const str = 'javascript is fun'
 
 const res = str.charAt(0).toUpperCase() + str.slice(1);
 console.log(res);
+
+// or
+
+let res1 = str[0].toUpperCase();
+
+for(let i = 1; i < str.length; i++){
+  res1 += str[i];
+}
+console.log(res1);
+
