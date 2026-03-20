0d52d04f5a2a4878012aad6dee65d6bac21b45a8
0xWares <=md.abdulwares@outlook.com>
2026-03-20 10:47:50 +0600

    longest string problem solved

---
+++ problem9.js
@@ -0,0 +1,20 @@
+const str = "hello world! I love programming..."
+const result = str.split(" ").reduce((longest, current) => {
+  return current.length > longest.length ? current : longest;
+
+});
+console.log(result);
+
+// or
+
+let words = str.split(" ");
+
+let longest = words[0];
+
+for(let w of words){
+  if(w.length > longest.length){
+    longest = w;
+  }
+}
+
+console.log(longest);
