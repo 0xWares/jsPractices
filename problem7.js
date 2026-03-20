e4064b4c5e710cfeb8ec9f15469ac1ea76433580
0xWares <md.abdulwares.outlook.com>
2026-03-20 10:24:24 +0600

    Problem 7 and 8 has been solved

--- problem7.js
+++ problem7.js
@@ -1,3 +1,8 @@
-const name = 'wares';
-const age = 50;
-console.log(name + age);
+// Check if any element is greater then 100 using the some method
+
+
+const arr = [23, 35, 150, 10];
+
+const hasBigNum = arr.some(x => x > 100);
+
+console.log(hasBigNum);
